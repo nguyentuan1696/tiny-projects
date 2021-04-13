08c3220fd7d2af8ab273145f65ab09fb4e58a284
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-04-13 08:20:21 +0700

    update

---
+++ react-projects/starter-project-redux-tutorial-cart/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+// components
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+// items
+import cartItems from './cart-items'
+// redux stuff
+import { createStore } from 'redux'
+
+import {DECREASE, INCREASE, RESET, CHANGE_NAME} from './actions'
+
+const initialState = {
+  count: 0,
+  name: 'john'
+}
+
+
+
+const reducer = (state, action) => {
+  console.log({ state, action })
+  if (action.type === DECREASE) {
+    return {...state, count: state.count - 1, name: 'anna' }
+  }
+
+  if (action.type === INCREASE) {
+    return {...state, count: state.count + 1 }
+  }
+  if (action.type === RESET) {
+    return { ...state,count: 0 }
+  }
+  if (action.type === CHANGE_NAME) {
+    return {...state, name: 'bobo'}
+  }
+  return state
+}
+
+const store = createStore(reducer, initialState)
+store.dispatch({ type: DECREASE })
+store.dispatch({ type:  INCREASE})
+store.dispatch({ type: CHANGE_NAME })
+store.dispatch({ type: RESET})
+console.log(store.getState())
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar cart={store.getState()} />
+      <CartContainer cart={cartItems} />
+    </main>
+  )
+}
+
+export default App
