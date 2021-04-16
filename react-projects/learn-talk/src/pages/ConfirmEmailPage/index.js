b70207290d43d14602a971b55a206022be34684e
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-04-16 16:55:36 +0700

    update

--- react-projects/learn-talk/src/pages/ConfirmEmailPage/index.js
+++ react-projects/learn-talk/src/pages/ConfirmEmailPage/index.js
@@ -9,7 +9,6 @@ import Validate from '../../utilities/Validate'
 const ConfirmEmailPage = () => {
   const [error, setError] = useState('')
   const [data, setData] = useState('')
-
   const [isLogin, setLogin] = useState('')
 
   const [otpArr, setOTP] = useState([])
@@ -27,40 +26,209 @@ const ConfirmEmailPage = () => {
   const inputE5 = useRef(null)
   const inputE6 = useRef(null)
 
+  const loginByOTP2 = (otp) =>
+  {
+    setError('')
+    if (!otp) {
+      setError('Vui lòng nhập mã xác thực.')
+      Validate.sendError(
+        'confirmEmail',
+        accProps,
+        window.location.href,
+        Constant.API_BASE_URL + 'confirmAccount',
+        '',
+        'Vui lòng nhập mã xác thực.'
+      )
+      return
+    }
+
+    let data = JSON.stringify({
+      acc: accProps,
+      otp: otp,
+      password: '*****'
+    })
+
+    let dataKibana = JSON.stringify({
+      acc: accProps,
+      otp: otp,
+      password: '*****',
+    })
+  }
+
+  const handleCode1 = (e) => {
+    setError('')
+    if (e.length === 1) {
+      setCode1(e)
+      inputE2.current.focus()
+    }
+
+    if (e.length === 6) {
+      setCode1(e.slice(0, 1))
+      setCode2(e.slice(1, 2))
+      setCode3(e.slice(2, 3))
+      setCode4(e.slice(3, 4))
+      setCode5(e.slice(4, 5))
+      setCode6(e.slice(5, 6))
+
+      inputE6.current.focus()
+      loginByOTP2(e)
+    }
+  }
+
+  let accProps = ''
+  let fromProps = ''
+  let hasMobileProps = ''
+  let isVietID = ''
+
   const initialTime = 30 * 1000
   const interval = 1000
-  const [timeLeft, {start}] = useCountDown(initialTime, interval)
-  useEffect(() =>
-  {
+  const [timeLeft, { start }] = useCountDown(initialTime, interval)
+  useEffect(() => {
     start()
   }, [])
 
-  const restart = useCallback(() =>
-  {
+  const restart = useCallback(() => {
     const newTime = 30 * 1000
     start(newTime)
   }, [])
 
-  useEffect(() =>
-  {
+  useEffect(() => {
     const agent = window.navigator.userAgent
     if (agent.indexOf('Android') > -1) {
       window.sendActionToNative('show_keyboard')
       window.ee.addListener('show_keyboard', showKeyboard)
       window.KingIDSdk.onShowKeyBoard()
-      return(() => {window.KingIDSdk.onHideKeyboard()})
+      return () => {
+        window.KingIDSdk.onHideKeyboard()
+      }
     }
   })
 
-  const showKeyboard = (callback) =>
-  {
+  const showKeyboard = (callback) => {
     if (callback === -1 || callback === '-1') {
-      setOTP(otpArr => otpArr.slice(0, otpArr.length -1))
+      setOTP((otpArr) => otpArr.slice(0, otpArr.length - 1))
+    } else {
+      setOTP((otpArr) => {
+        return otpArr.length < 6 ? otpArr.concat(callback) : otpArr
+      })
     }
   }
 
+  const hideKeyBoard = () => {
+    const agent = window.navigator.userAgent
+    if (agent.indexOf('Android') > -1) {
+      window.KingIDSdk.onHideKeyBoard()
+      window.ee.removeListener('show_keyboard')
+    }
+  }
 
-  return <h1>ConfirmEmailPage</h1>
+  const onPaste = () => {}
+
+  const handleKeyUp = () => {}
+
+  const showInputOTP = () => {
+    let agent = window.navigator.userAgent
+    if (agent.indexOf('Android') === -1) {
+      return (
+        <div className='wrap-digit'>
+          <input
+            type='tel'
+            pattern='\d*'
+            className='input-digit'
+            onPaste={onPaste}
+            ref={inputE1}
+            value={code1}
+            autoFocus
+            autoComplete='one-time-code'
+            id='single-factor-code-text-field'
+            onChange={(e) => handleCode1(e.target.value)}
+            onKeyUp={handleKeyUp}
+          />
+        </div>
+      )
+    }
+  }
+
+  const showError = () => {
+    if (error !== null) {
+      return <div className='text-orange font15 marginT16'>{error}</div>
+    }
+  }
+
+  let dataState = {
+    acc: accProps,
+    from: fromProps,
+  }
+
+  if (data.signal === 1) {
+    if (fromProps === 'enterPassword') {
+      return (
+        <Redirect to={{ pathname: '/talk/setPassword', state: dataState }} />
+      )
+    }
+
+    if (hasMobileProps && fromProps === 'index' && isLogin.signal === 1) {
+      return window.location.replace(
+        isLogin.data ? isLogin.data : Constant.REDIRECT_URL
+      )
+    }
+
+    return <Redirect to={{ pathname: '/talk/addMobile', state: dataState }} />
+  } else {
+    const back =
+      fromProps === 'enterPassword' ? (
+        <Link
+          className='img-back'
+          to='/talk/enterPassword'
+          onClick={() => hideKeyBoard}
+        >
+          <img
+            src='https://mingid.mediacdn.vn/king/image/back.png'
+            alt=''
+          ></img>
+        </Link>
+      ) : (
+        <Link
+          className='img-back'
+          to={{ pathname: '/talk/index' }}
+          onClick={() => hideKeyBoard()}
+        >
+          <img
+            src='https://mingid.mediacdn.vn/king/image/back.png'
+            alt=''
+          ></img>
+        </Link>
+      )
+
+    let classImg = isVietID ? (
+      <div className='img-logo marginT64'>
+        <img
+          src='https://mingid.mediacdn.vn/king/image/logo-vietid.png'
+          alt=''
+        ></img>
+      </div>
+    ) : (
+      ''
+    )
+
+    let classTitle = isVietID ? 'title marginT24' : 'title'
+
+    return (
+      <div className='wrapper-enter-otp'>
+        {back}
+        {classImg}
+        <div className='classTitle'>
+          Nhập mã xác thực được gửi về Email <span>{accProps}</span>
+        </div>
+
+        <div className='wrapper-input'>
+          <div className='item'>{showInputOTP}</div>
+        </div>
+
+        {showError}
+      </div>
+    )
+  }
 }
 
 export default ConfirmEmailPage
